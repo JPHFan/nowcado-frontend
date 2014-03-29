06ccc8d3f0873e568ac445c56229b319240b4c60
Benjamin Tousley <ben@Ben-CentOS.(none)>
2014-03-29 15:17:08 -0600

    Explicitly login when FB icon is clicked.

--- public/js/app.js
+++ public/js/app.js
@@ -89,14 +89,14 @@ $("#sign_up_submit").click(function(e) {
   );
 });
 
-$("#facebook_login_icon").click(function(e) {
-  FB.init({
-    appId      : '498305683523319',
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-  });
+FB.init({
+  appId      : '498305683523319',
+  status     : true, // check login status
+  cookie     : true, // enable cookies to allow the server to access the session
+  xfbml      : true  // parse XFBML
+});
 
+$("#facebook_login_icon").click(function(e) {
   // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
   // for any authentication related change, such as login, logout or session refresh. This means that
   // whenever someone who was previously logged out tries to log in again, the correct case below
@@ -117,14 +117,15 @@ $("#facebook_login_icon").click(function(e) {
       // (1) JavaScript created popup windows are blocked by most browsers unless they
       // result from direct interaction from people using the app (such as a mouse click)
       // (2) it is a bad experience to be continually prompted to login upon page load.
-      FB.login();
+      //FB.login();
     } else {
       // In this case, the person is not logged into Facebook, so we call the login()
       // function to prompt them to do so. Note that at this stage there is no indication
       // of whether they are logged into the app. If they aren't then they'll see the Login
       // dialog right after they log in to Facebook.
       // The same caveats as above apply to the FB.login() call here.
-      FB.login();
+      //FB.login();
     }
   });
+  FB.login();
 });
