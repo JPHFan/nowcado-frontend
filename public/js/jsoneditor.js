b5db9ef512a7d9c017948f3e194bd7442c0e58fd
Avi <avibardack@gmail.com>
2014-07-21 17:11:31 -0400

    Remove check to see if department has not changed.

--- public/js/jsoneditor.js
+++ public/js/jsoneditor.js
@@ -1176,10 +1176,10 @@ jsonEditor = {
     }
     if(jE.evalNewJson() == -1) return -1;
     // Check if this object is the same. If so, alert there was no change and return -1.
-    if(JSON.stringify(jE.jsonObj).replace(/\"/g,"") == $("ul#dept_datetime_list li:first a").attr("dept")) {
-      $.growl("You did not change the department.",growl_resp.pass);
-      return -1;
-    }
+    // if(JSON.stringify(jE.jsonObj).replace(/\"/g,"") == $("ul#dept_datetime_list li:first a").attr("dept")) {
+    //   $.growl("You did not change the department.",growl_resp.pass);
+    //   return -1;
+    // }
     f();
   }
 };
